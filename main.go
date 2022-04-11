8a322613f69702f1373217af31a46e4df747f88a
andtyr <twiss312@gmail.com>
2022-04-11 00:55:57 -0700

    Comploot

--- main.go
+++ main.go
@@ -4,6 +4,35 @@ import (
 	"fmt"
 )
 
-func main () {
-	fmt.Println("hello-world")
+func main() {
+	/*fmt.Println("hello-world")
+
+	greetinText := "Hello World!!!!!"
+	luckyNumber := 69
+	evenMoreLuckyNumber := luckyNumber - 10
+
+	fmt.Println(greetinText)
+	fmt.Println(luckyNumber)
+	fmt.Println(evenMoreLuckyNumber)
+
+	evenMoreLuckyNumber = luckyNumber * 3
+
+	fmt.Println(evenMoreLuckyNumber)
+	*/
+
+	firstName := "Nunya"
+	lastName := "Bidniss"
+	currentYear := 2022
+	birthYear := 1922
+	nextYear := currentYear + 1
+	age := currentYear - birthYear
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(currentYear)
+	fmt.Println(birthYear)
+	fmt.Println(nextYear)
+	fmt.Println(age)
+	fmt.Println()
+
 }

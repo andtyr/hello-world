64c6c7107c7838948342799f639f851407b0b6f3
andtyr <twiss312@gmail.com>
2022-04-13 22:28:19 -0700

    Donezo

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	/*fmt.Println("hello-world")
+	fmt.Println("hello-world")
 
 	greetinText := "Hello World!!!!!"
 	luckyNumber := 69
@@ -18,11 +18,11 @@ func main() {
 	evenMoreLuckyNumber = luckyNumber * 3
 
 	fmt.Println(evenMoreLuckyNumber)
-	*/
 
 	firstName := "Nunya"
 	lastName := "Bidniss"
-	currentYear := 2022
+	var currentYear int = 2022
+	currentYear = currentYear + 1
 	birthYear := 1922
 	nextYear := currentYear + 1
 	age := currentYear - birthYear
@@ -33,6 +33,42 @@ func main() {
 	fmt.Println(birthYear)
 	fmt.Println(nextYear)
 	fmt.Println(age)
-	fmt.Println()
 
+	var newNumber float64 = float64(luckyNumber) / 4
+
+	fmt.Println(newNumber)
+
+	var defaultFloat float64 = 1.23456789123456789
+	var smallFloat float32 = 1.23456789123456789
+
+	fmt.Println(defaultFloat)
+	fmt.Println(smallFloat)
+
+	var firstRune rune = '€'
+	fmt.Println(firstRune)
+	fmt.Println(string(firstRune))
+
+	//Output as numeral '97', which corresponds to char value.
+	var firstByte byte = 'a'
+	fmt.Println(firstByte)
+
+	//Example of combining strings. Whitespace must be specified with either '.' or [string] + " " + [string].
+	fmt.Println(firstName, lastName)
+
+	fullName := (firstName + " " + lastName)
+
+	fmt.Printf("Hi, I am %v and I am %v years old\n", fullName, age)
+
+	var pi float32 = 3.14
+	fmt.Println(pi)
+
+	var circleRadius int = 5
+	fmt.Println(circleRadius)
+
+	newFormula := (2 * pi * float32(circleRadius))
+	fmt.Println(newFormula)
+
+	fmt.Printf("For a radius of %v, the circle Circumference is %.2f", circleRadius, newFormula)
+
+	//(Type %T) to come
 }
